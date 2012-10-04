c91abd21c00bce03568c03f9eca564a58c8cf141
Markus Felten <Markus.Felten@gmx.de>
2012-10-04 20:02:55 +0200

    ongoing work

--- entitlement.js
+++ entitlement.js
@@ -3,16 +3,32 @@
 
 var async    = require("async");
 
+
 var RootEntitlement = {
 	"toString" : function() { return this.id + ": " + this.description['en']; }
 };
 
 function Entitlement(id,options)
 {
-	return Object.create(RootEntitlement,{
-		'id' : { value : id },
-		'description' : { value: options.description }
-	});
+	var defs = { id: {value: id, enumerable: true }};
+	var key;
+
+	for(key in options) {
+		if(options.hasOwnProperty(key)) {
+			defs[key] = {value: options[key], enumerable: true };
+		}
+	}
+
+	return Object.create(RootEntitlement, defs);
+}
+
+var _entitlements = {};
+
+function Entitlements(ents)
+{
+	for(var i in ents) {
+		_entitlements[i] = Entitlement(i, ents[i]);
+	}
 }
 
 function Role(id,descriptions)
@@ -20,5 +36,12 @@ function Role(id,descriptions)
 	
 }
 
+function Roles(roles)
+{
+	
+}
+
 exports.Entitlement   = Entitlement;
+exports.Entitlements  = Entitlements;
 exports.Role          = Role;
+exports.Roles         = Roles;
